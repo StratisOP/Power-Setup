74a2e14c18e0c08ee7fd03c58224aac5e6680acc
StratisOP <stratis@protonmail.com>
2022-01-13 09:49:05 +0200

    1.1.3.0

--- PowerLangSetup.ps1
+++
@@ -1,6 +0,0 @@
-#Set Language, region, and keyboard languages
-Set-Culture en-US
-Set-WinUILanguageOverride -Language en-US
-Set-WinUserLanguageList en-US, el-GR -Force
-Set-WinHomeLocation -GeoId 98
-Set-WinSystemLocale -SystemLocale el-GR
--- PowerNetSetup.ps1
+++
@@ -1,6 +0,0 @@
-#Enable firewall rule for Remote Desktop - User Mode (TCP & UDP)
-Get-NetFirewallRule -DisplayName "Remote Desktop - User*" | Set-NetFirewallRule -Enabled true
-#Enable firewall rule for Remote Desktop - User Mode (TCP & UDP)
-Get-NetFirewallRule -DisplayName "Απομακρυσμένη επιφάνεια εργασίας - Λειτουργία χρήστη*" | Set-NetFirewallRule -Enabled true
-#Disable Automatically Detect proxy settings
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" AutoDetect 0
